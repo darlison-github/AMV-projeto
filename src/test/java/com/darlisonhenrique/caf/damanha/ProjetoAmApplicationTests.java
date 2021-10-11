aa3ad90ad75ad4ae1622b64065e278b525c279d9
darlison-github/Projetos <darlison-henrique@hotmail.com>
2021-10-11 18:51:20 -0300

    parte 1

---
+++ src/test/java/com/darlisonhenrique/caf/damanha/ProjetoAmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darlisonhenrique.caf.damanha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
